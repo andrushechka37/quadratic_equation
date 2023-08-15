ceed434150622c44c55cb9ee70a6cd10da006a40
andrushechka37 <142117922+andrushechka37@users.noreply.github.com>
2023-08-15 19:50:58 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+int main(void) {
+    double a, b, c, x, d;
+    printf("print a, b, c from ax^2 + bx + c = 0\n");  //ввод данных
+    printf("a = ");
+    scanf("%lf", &a);
+    printf("b = ");
+    scanf("%lf", &b);
+    printf("c = ");
+    scanf("%lf", &c);
+
+    if (a == 0) {
+        if (b != 0) {
+            x = (double) (-c / b);
+            printf("x1 = x2 = %lf", x);
+        } else {
+            printf("an infinite number of solutions");
+        }
+    } else {
+        d = b * b - 4 * a * c;
+        if (d < 0) {
+            printf("no valid solutions");
+        } else if (d == 0) {
+            x = (double) (-b / (2 * a));
+            printf("x1 = x2 = %lf", x);
+        } else {
+            x = (double) (-b + sqrt(d)) / (2 * a);
+            printf("x1 = %lf\n", x);
+            x = (double) (-b - sqrt(d)) / (2 * a);
+            printf("x2 = %lf\n", x);
+        }
+    }
+    return 0;
+}
