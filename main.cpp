f866e4fdd5bf70c4433ccf020576b07549dd2931
andrushechka365 <142117922+andrushechka365@users.noreply.github.com>
2023-08-17 20:01:05 +0300

    отделено решение от интерфейса

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
-int main(void) { 
+void solve (double frst_coef, double sec_coef, double free_member);
+int main(void) {
 
 
-    double frst_coef, sec_coef, free_member; 
+    double frst_coef, sec_coef, free_member;
     printf("print a, b, c from ax^2 + bx + c = 0\n");
     printf("a = ");
-    scanf("%lf", &frst_coef); 
-    printf("b = "); 
-    scanf("%lf", &sec_coef); 
-    printf("c = ");  
-    scanf("%lf", &free_member); 
+    scanf("%lf", &frst_coef);
+    printf("b = ");
+    scanf("%lf", &sec_coef);
+    printf("c = ");
+    scanf("%lf", &free_member);
 
-    double x1;
+    solve(frst_coef, sec_coef, free_member);
+    return 0;
+}
+
+void solve (double frst_coef, double sec_coef, double free_member) {
+        double x1;
     double epsilon = 1e-9;
-    if ((frst_coef >= (-epsilon)) && (frst_coef <= epsilon)) { 
-        if ((sec_coef <= (-epsilon)) || (sec_coef >= epsilon)) { 
+    if ((frst_coef >= (-epsilon)) && (frst_coef <= epsilon)) {
+        if ((sec_coef <= (-epsilon)) || (sec_coef >= epsilon)) {
             x1 = (double) (-free_member / sec_coef);
             printf("x1 = x2 = %.2lf", x1);
         } else if (((sec_coef >= (-epsilon)) && (sec_coef <= epsilon)) && ((free_member >= (-epsilon)) && (free_member <= epsilon))) {
@@ -37,5 +43,4 @@ int main(void) {
             printf("x1 = %.2lf\nx2 = %.2lf", x1, x2);
         }
     }
-    return 0;
 }
